e1eaaf01edbb10e59e8b7fb014f335f356a20705
melisa87 <melisa.bekan@hotmail.com>
2021-04-13 21:23:44 +0200

    Changes in order to compplete Task 1

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=proj-docker-kuber . 
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -d -p 8080:8080 proj-docker-kuber
